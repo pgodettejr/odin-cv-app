8c5ba5df3efc61828305ee56439249378d39c870
pgodettejr <109110310+pgodettejr@users.noreply.github.com>
2025-07-25 20:21:03 -0400

    Add more dev comments related to next steps
    Wrote some more comments just dumping additional thoughts about how to
    approach the React side of the project. The JSX may need a rework based
    on going back over the React docs a second time.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,20 @@ import './styles/App.css'
 
 // TODO: Look at Rendering Techniques lesson and focus on 'conditional rendering' docs for rendering the form inputs as HTML elements when the form is submitted via the Submit button. Put the logic in a separate JSX/components file or directory
 
+// Use console.log...a lot (confirm React batching state updates on re-renders)
+
+// Might need to do CRUD functions? The app needs to be able to read the inputs in the form and either change them to HTML elements (Submit) or read the HTML elements and change them back to form inputs (Edit). We SHOULD try to write CONTROLLED components for the form elements instead...we might need both controlled components AND the CRUD functions
+
+// An example of a controlled component:
+// const [value, setValue] = useState("");
+    // For 'input' elements below: value={value}, onChange={(e or event) => setValue(e or event.target.value)}
+
+// DOCS 
+// Rendering Techniques - focus on conditional rendering docs for rendering form inputs as HTML on Submit. Put logic into separate JSX files
+// State: A Component's Memory (Challenges) - reference to how we should use React to do our forms? there are states for every input though
+// State as a Snapshot - entire doc has more examples of forms & form structure
+// Choosing the State Structure - see the first TODO in the App function below for grouping, state contradictions form example
+
 // BEGIN
 // WHEN the user goes to the website/browser app via web address
 // DISPLAY the entire application
@@ -16,7 +30,11 @@ import './styles/App.css'
 // const toggleResumeForm = ["incomplete", "complete"]; 
 // const initialFormState = "";
 
+// Do we need to pass a callback to our "setResumeForm" function in our "handle" function for Edit/Submit buttons? (More on State lesson)
+// setResumeForm((prevForm) => ({...prevForm, ???}));
+
 function App() {
+  // We likely only need to declare one state for the entire form, not multiple states per section
   const [count, setCount] = useState(0)
 
   // STATES
@@ -27,6 +45,8 @@ function App() {
 
   // Not sure what we would use this for, come back to it later
   // TODO: Rename the 'useState' argument (formInput, formState, false, etc.)
+  // For the useState prop/argument, if I'm only using one single state, will I need to make an array with nested objects for the entire form with each object containing the property/value representing each input? .map thru it? [{title: name, id: 0}, {title: location, id: 1}, etc.]
+  // Group into a single state if we don't know how many pieces of state we need (forms where user adds custom fields). If we only need to set one field alone, do "setResumeForm{...form, fieldName: userInput}"
   // const [resumeForm, setResumeForm] = useState(toggleResumeForm[0] OR initialFormState);
 
   // TODO: Write a function or component both for the Edit and for the Submit button that returns an event listener that runs the code for each button respectively. Utilize 'useState' to change the state of the app when each button is interacted with/clicked
@@ -61,6 +81,7 @@ function App() {
         {/* May need to change single quotation tags to double quotations if error shows up for them */}
         {/* Put each section of the form in their own div wrappers/containers if needed  */}
         {/* camelCase the names of our HTML attributes (that are in quotations) if we need to  */}
+        {/* May need an 'onSubmit' event for this form element tag: onSubmit={handleSubmit[Button?]}. Should also add preventDefault. */}
         <form action="" method="get" id='cv-form'>
 
           {/* DISPLAY the Name, Email & Phone # form inputs in the Personal Info section */}
